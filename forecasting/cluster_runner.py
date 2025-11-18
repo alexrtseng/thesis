27110bc0d4a7bc390355eba3cc7babeadc29062d
Alex Tseng <134972440+alexrtseng@users.noreply.github.com>
2025-11-17 23:43:55 -0500

    accept legacy

--- forecasting/cluster_runner.py
+++ forecasting/cluster_runner.py
@@ -40,7 +40,7 @@ import torch
 
 from forecasting.model_zoo import ModelName, make_registry
 from forecasting.sweep_runner import build_series_for_node, run_sweep_for_node
-torch.backends.cuda.matmul.fp32_precision = 'tf32'
+torch.set_float32_matmul_precision("high")
 
 def _parse_models(raw: str) -> List[ModelName]:
     reg = make_registry()
