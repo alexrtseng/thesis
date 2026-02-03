77502728b3c2c53049e8a3b34cf31ff923ffd194
Alex Tseng <134972440+alexrtseng@users.noreply.github.com>
2026-02-03 00:29:53 -0500

    Uncomment all forecasters

--- forecasting/test_best_model_combinations.py
+++ forecasting/test_best_model_combinations.py
@@ -18,22 +18,22 @@ TEST_HF_MODEL_RUNS = [
     "watt-our/thesis-hf-forecasters/obvw0nqv",  # transformer
     "watt-our/thesis-hf-forecasters/ttzome1t",  # nhits
     "watt-our/thesis-hf-forecasters/j7oviesl",  # transformer
-    # "watt-our/thesis-hf-forecasters/mh9t0r9v",  # tsmixer
-    # "watt-our/thesis-hf-forecasters/wtdafv56",  # transformer
-    # "watt-our/thesis-hf-forecasters/2keubomv",  # blockrnn
-    # "watt-our/thesis-hf-forecasters/2ydmchx4",  # tsmixer
-    # "watt-our/thesis-hf-forecasters/xl7ier6h",  # dlinear
-    # "watt-our/thesis-hf-forecasters/lkujq05c",  # nhits
-    # "watt-our/thesis-hf-forecasters/aclpbztf",  # nbeats
-    # "watt-our/thesis-hf-forecasters/y3s9jm3u",  # tide
-    # "watt-our/thesis-hf-forecasters/zu09p08u",  # RNN
-    # "watt-our/thesis-hf-forecasters/qm3no329",  # TCN
-    # "watt-our/thesis-hf-forecasters/d7odgxif",  # RNN
-    # "watt-our/thesis-hf-forecasters/wqipc7pb",  # xgb
-    # "watt-our/thesis-hf-forecasters/a2t6syeh",  # tcn
-    # "watt-our/thesis-hf-forecasters/hrx8q1e6",  # xgb
-    # "watt-our/thesis-hf-forecasters/wa2txvsd",  # tft
-    # "watt-our/thesis-hf-forecasters/n9468o4a",  # autoarima
+    "watt-our/thesis-hf-forecasters/mh9t0r9v",  # tsmixer
+    "watt-our/thesis-hf-forecasters/wtdafv56",  # transformer
+    "watt-our/thesis-hf-forecasters/2keubomv",  # blockrnn
+    "watt-our/thesis-hf-forecasters/2ydmchx4",  # tsmixer
+    "watt-our/thesis-hf-forecasters/xl7ier6h",  # dlinear
+    "watt-our/thesis-hf-forecasters/lkujq05c",  # nhits
+    "watt-our/thesis-hf-forecasters/aclpbztf",  # nbeats
+    "watt-our/thesis-hf-forecasters/y3s9jm3u",  # tide
+    "watt-our/thesis-hf-forecasters/zu09p08u",  # RNN
+    "watt-our/thesis-hf-forecasters/qm3no329",  # TCN
+    "watt-our/thesis-hf-forecasters/d7odgxif",  # RNN
+    "watt-our/thesis-hf-forecasters/wqipc7pb",  # xgb
+    "watt-our/thesis-hf-forecasters/a2t6syeh",  # tcn
+    "watt-our/thesis-hf-forecasters/hrx8q1e6",  # xgb
+    "watt-our/thesis-hf-forecasters/wa2txvsd",  # tft
+    "watt-our/thesis-hf-forecasters/n9468o4a",  # autoarima
 ]
 
 TEST_LF_MODEL_RUNS = [
@@ -41,22 +41,22 @@ TEST_LF_MODEL_RUNS = [
     "watt-our/thesis-lf-forecasters/a3ilhshd",  # XGB
     "watt-our/thesis-lf-forecasters/4b1o2o11",  # tsmixer
     "watt-our/thesis-lf-forecasters/9je5svmu",  # xgboost
-    # "watt-our/thesis-lf-forecasters/60zx0lhp",  # xgboost
-    # "watt-our/thesis-lf-forecasters/epkknrba",  # xgboost
-    # "watt-our/thesis-lf-forecasters/k9jkje7f",  # xgboost
-    # "watt-our/thesis-lf-forecasters/hk4zyw7a",  # xgboost
-    # "watt-our/thesis-lf-forecasters/gwwjxk6g",  # rnn
-    # "watt-our/thesis-lf-forecasters/1grl6iaf",  # xgb
-    # "watt-our/thesis-lf-forecasters/j0r01ir1",  # tcn
-    # "watt-our/thesis-lf-forecasters/lx82mfuf",  # tide
-    # "watt-our/thesis-lf-forecasters/zqd9zf36",  # rnn
-    # "watt-our/thesis-lf-forecasters/75610i3y",  # tft
-    # "watt-our/thesis-lf-forecasters/2jcdrsn8",  # nhits
-    # "watt-our/thesis-lf-forecasters/db1wpxb6",  # blockrnn
-    # "watt-our/thesis-lf-forecasters/bpp4kwd9",  # dlinear
-    # "watt-our/thesis-lf-forecasters/iwvd9kfh",  # nbeats
-    # "watt-our/thesis-lf-forecasters/fl8aigij",  # transformer
-    # "watt-our/thesis-lf-forecasters/0c7iapks",  # autoarima
+    "watt-our/thesis-lf-forecasters/60zx0lhp",  # xgboost
+    "watt-our/thesis-lf-forecasters/epkknrba",  # xgboost
+    "watt-our/thesis-lf-forecasters/k9jkje7f",  # xgboost
+    "watt-our/thesis-lf-forecasters/hk4zyw7a",  # xgboost
+    "watt-our/thesis-lf-forecasters/gwwjxk6g",  # rnn
+    "watt-our/thesis-lf-forecasters/1grl6iaf",  # xgb
+    "watt-our/thesis-lf-forecasters/j0r01ir1",  # tcn
+    "watt-our/thesis-lf-forecasters/lx82mfuf",  # tide
+    "watt-our/thesis-lf-forecasters/zqd9zf36",  # rnn
+    "watt-our/thesis-lf-forecasters/75610i3y",  # tft
+    "watt-our/thesis-lf-forecasters/2jcdrsn8",  # nhits
+    "watt-our/thesis-lf-forecasters/db1wpxb6",  # blockrnn
+    "watt-our/thesis-lf-forecasters/bpp4kwd9",  # dlinear
+    "watt-our/thesis-lf-forecasters/iwvd9kfh",  # nbeats
+    "watt-our/thesis-lf-forecasters/fl8aigij",  # transformer
+    "watt-our/thesis-lf-forecasters/0c7iapks",  # autoarima
 ]
 
 
